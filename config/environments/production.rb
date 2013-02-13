020e7fe40a826f56f0c05a9d35c058f3ea0d5fc2
Brandon Rice <brice84@gmail.com>
2013-02-13 17:06:33 -0500

    Config fix.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,8 @@ Squash::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  # config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
