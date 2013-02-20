0dcbf28d3a05e26eb922986a3b7e56dfcf7e4f52
Brandon Rice <brice84@gmail.com>
2013-02-20 13:21:07 -0500

    Config hash access bug fix.

--- config/initializers/amazon_ses.rb
+++ config/initializers/amazon_ses.rb
@@ -3,6 +3,6 @@ if Rails.env == 'production'
   config = YAML.load_file(rails_root.to_s + '/config/amazon_ses.yml')
 
   ActionMailer::Base.add_delivery_method :ses, AWS::SES::Base,
-    :access_key_id      => config[Rails.env].id,
-    :secret_access_key  => config[Rails.env].key
+    :access_key_id      => config[Rails.env]['id'],
+    :secret_access_key  => config[Rails.env]['key']
 end
