f7efd34438660a35aeb13e0fb3ae3a4ac73bc5cb
Bo Jeanes <bjeanes@groupon.com>
2013-03-12 10:55:28 -0500

    Add script directory to warbler bundle
    Thanks to jruby/warbler#141, this will allow us to do the following in
    order to start a console, run a rake task, etc. directly from the WAR
    file:
    
        java -jar squash.war -S script/rails console

--- config/warble.rb
+++ config/warble.rb
@@ -11,7 +11,7 @@ Warbler::Config.new do |config|
   config.features = %w(executable runnable)
 
   # Application directories to be included in the webapp.
-  config.dirs = %w(app bin config data db lib log vendor tmp)
+  config.dirs = %w(app bin config data db lib log script vendor tmp)
 
   # Additional files/directories to include, above those in config.dirs
   # config.includes = FileList["db"]
