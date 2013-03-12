65b6de2b076bc1bc929fe43fe24bfda635b57391
Bo Jeanes <bjeanes@groupon.com>
2013-03-12 14:12:07 -0500

    Make session secret configurable through Configoro

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -18,4 +18,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Squash::Application.config.secret_token = '_SECRET_'
+Squash::Application.config.secret_token = Squash::Configuration.application.secret_token || '_SECRET_'
