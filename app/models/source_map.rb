5f7a17a15e84cfe22c0f923107deed6181387daf
Tim Morgan <tim@squareup.com>
2013-03-22 17:54:00 -0700

    Add missing licenses

--- app/models/source_map.rb
+++ app/models/source_map.rb
@@ -4,13 +4,13 @@
 #    you may not use this file except in compliance with the License.
 #    You may obtain a copy of the License at
 #
-#        http://www.apache.org/licences/LICENSE-2.0
+#        http://www.apache.org/licenses/LICENSE-2.0
 #
 #    Unless required by applicable law or agreed to in writing, software
-#    distributed under the Licence is distributed on an "AS IS" BASIS,
+#    distributed under the License is distributed on an "AS IS" BASIS,
 #    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#    See the Licence for the specific language governing permission and
-#    limitation under the License.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
 
 # Source mapping information for a JavaScript project. This model stores a
 # mapping of minified JavaScript code location and symbol names to
